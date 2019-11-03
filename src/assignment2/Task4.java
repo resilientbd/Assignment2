4e330dc68e3bcba840c8cfd7e493a4a4f1add05e
Admin <Admin@DESKTOP-MRPLHGR>
2019-11-03 14:02:33 +0600

    task3 and 4 done

--- src/assignment2/Task4.java
+++ src/assignment2/Task4.java
@@ -12,13 +12,14 @@ public class Task4 {
         int a,b,c;
         System.out.print("Enter first Value = ");
         a=input.nextInt();
-         System.out.println("Enter Second Value = ");
+         System.out.print("Enter Second Value = ");
         b= input.nextInt();
         c=a+b;
         int d=a*b;
         int e=a-b;
         System.out.println("Sum = "+c);
-        System.out.println("Pro");
+        System.out.println("Product = "+d);
+        System.out.println("Diffrence = "+e);
         
     }
     
