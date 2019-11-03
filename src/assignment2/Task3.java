265254739560e24cdd9a609ef3618acbd69aaa49
Admin <Admin@DESKTOP-MRPLHGR>
2019-11-03 13:19:00 +0600

    task3 and 4 done

---
+++ src/assignment2/Task3.java
@@ -0,0 +1,18 @@
+
+package assignment2;
+
+import java.util.Scanner;
+
+
+public class Task3 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter any value =  ");
+        int A= input.nextInt();
+        System.out.println("Value of = "+A);
+                
+    }
+
+    
+}
---
+++ src/assignment2/Task4.java
@@ -0,0 +1,25 @@
+
+package assignment2;
+
+import java.util.Scanner;
+
+
+public class Task4 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        int a,b,c;
+        System.out.print("Enter first Value = ");
+        a=input.nextInt();
+         System.out.println("Enter Second Value = ");
+        b= input.nextInt();
+        c=a+b;
+        int d=a*b;
+        int e=a-b;
+        System.out.println("Sum = "+c);
+        System.out.println("Pro");
+        
+    }
+    
+}
