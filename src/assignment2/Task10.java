824c4e110e29594ed0bad60528c00b1692f92e8e
AMITABH <AMITABH@DESKTOP-948PNL0>
2019-11-03 16:01:41 +0600

    10 is done

---
+++ src/assignment2/Task10.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author AMITABH
+ */
+public class Task10 {
+    public static void main(String[] args) {
+   
+        System.out. println("Enter the Number a: ");
+        Scanner in = new Scanner(System.in);
+        int a = in. nextInt();
+        if (a/2==0){
+             System.out. println(" a is even ");
+        }
+        else{
+             System.out. println("a is odd ");
+        }
+    }
+    
+}
