fc93fc969f50337ed152b790cb68867415bfc663
AMITABH <AMITABH@DESKTOP-948PNL0>
2019-11-03 16:01:17 +0600

    9 is done

--- src/assignment2/Task9.java
+++ src/assignment2/Task9.java
@@ -5,10 +5,32 @@
  */
 package assignment2;
 
+import java.util.Scanner;
+
 /**
  *
  * @author AMITABH
  */
 public class Task9 {
+    public static void main(String[] args) {
+    System.out. println("Enter the Number a: ");
+        Scanner in = new Scanner(System.in);
+        int a = in. nextInt();
+        System.out. println("Enter the Number b: ");
+        int b = in. nextInt();
+        if (a>b)
+        {
+        System.out. println(" first number is greater  ");
+        int c;
+        c=a-b;
+        System.out. println(" Result "+c);
+        }
+        else
+        {
+          System.out. println(" second number is greater: ");
+          int c=b-a;
+          System.out. println(" Result "+c);
+        }
     
+    }
 }
