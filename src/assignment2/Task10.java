fbb43b36b8d3445d4927ec573852e9bc5192fe63
Admin <Admin@DESKTOP-7PHGEPF>
2019-11-03 17:14:24 +0600

    Task2 is complete

---
+++ src/assignment2/Task10.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Admin
+ */
+public class Task10 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a, b;
+        System.out.print("Enter ftrst Value");
+        a = input.nextInt();
+       if(a%2==0){
+           
+           System.out.println("Even");
+       }
+       else{
+           
+           System.out.println("Odd");
+       }
+
+    }
+}
