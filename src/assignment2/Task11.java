f02859870653df36f2bc10912bb35fb30b18da8a
Admin <Admin@DESKTOP-7PHGEPF>
2019-11-04 11:41:25 +0600

    All Project done

---
+++ src/assignment2/Task11.java
@@ -0,0 +1,14 @@
+package assignment2;
+
+import java.util.Scanner;
+
+public class Task11 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter any Value =  ");
+        int A = input.nextInt();
+        System.out.println("Value of = " + A);
+    }
+
+}
