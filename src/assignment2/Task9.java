05aff0896431eb6b704e685541a8196e233329d8
Admin <Admin@DESKTOP-Q265325>
2019-11-04 11:34:08 +0600

    10 projects done

---
+++ src/assignment2/Task9.java
@@ -0,0 +1,28 @@
+
+package assignment2;
+
+import java.util.Scanner;
+
+public class Task9 {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+          int c;
+        System.out.println("enter the number = ");
+        int a = input.nextInt();
+        int b = input.nextInt();
+        if (a > b) {
+            c = a - b;
+            System.out.println("the substract is = " + c);
+        }
+            else 
+    
+            {
+                    c=b-a;
+                    
+            
+            System.out.println("the substract is  "+c);
+
+        }
+    }
+    
+}
