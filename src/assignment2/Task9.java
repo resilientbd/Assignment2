0089458eae6cbc55bb8c113d8fb740b8f44545b2
user <user@DESKTOP-RP5RJAQ>
2019-11-04 08:12:27 +0600

    ( task5, task6, task9, task10 ) is done

---
+++ src/assignment2/Task9.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author user
+ */
+public class Task9 {
+    
+     public static void main (String[] args){
+    
+     Scanner scanner = new Scanner(System.in);
+     
+     int number1, number2, result;
+     
+     
+        System.out.println("Enter the first number: ");
+        number1 = scanner.nextInt();
+        
+        System.out.println("Enter the second number: ");
+        number2 = scanner.nextInt();
+        
+  
+        result = number1 - number2;
+        System.out.println("sum = " +result);
+        
+        
+        
+    }
+    
+}
