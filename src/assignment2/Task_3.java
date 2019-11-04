7863c17bb7acd2be6be35635e38d0bdfebba3661
RIYAD Patwary <RIYAD Patwary@ROG_STRIX_G731GV>
2019-11-05 01:18:57 +0600

    init repo

---
+++ src/assignment2/Task_3.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author RIYAD Patwary
+ */
+public class Task_3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /*
+        Write the Java code of a program that reads one number from the user, and prints it back to show which number was entered by the user.
+        */
+        
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Input a number: ");
+        int num1=sc.nextInt();
+        System.out.println("The number you inputed is "+num1);
+        
+    }
+    
+}
